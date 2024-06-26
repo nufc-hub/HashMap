5ce246fc204a9125c3b7f1a237521a8910ac5c49
nufc-hub <kmchenouf@gmail.com>
2024-06-26 23:14:24 +0900

    Add hash method

--- hash-map.js
+++ hash-map.js
@@ -2,4 +2,20 @@
 //   throw new Error("Trying to access index out of bound");
 // }
 
-class HashMap {}
+class HashMap {
+  hash(key, bucketSize) {
+    let hashCode = 0;
+
+    let primeNumber = 31;
+
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % bucketSize;
+    }
+
+    return hashCode;
+  }
+}
+
+const hashMap = new HashMap();
+
+console.log(hashMap.hash('karim', 13));
