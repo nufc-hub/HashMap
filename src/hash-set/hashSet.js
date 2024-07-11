133efc01b99d39985a64369cf86ee5cdbbc52f96
nufc-hub <kmchenouf@gmail.com>
2024-07-11 22:25:26 +0900

    Remove all key-value pair functionality

--- src/hash-set/hashSet.js
+++ src/hash-set/hashSet.js
@@ -1,12 +1,12 @@
 import LinkedList from './linkedList.js';
 
-class HashMap {
+class HashSet {
   constructor() {
     this.buckets = new Array(16).fill(null).map(() => new LinkedList());
     this.loadFactor = 0.75;
     // Capacity double each time there is a resize.
     this.capacity = this.buckets.length;
-    // Size increases each time a new key-value pair is set. Decreases if key-value pair deleted.
+    // Size increases each time a new key is set. Decreases if key deleted.
     this.size = 0;
   }
 
@@ -23,11 +23,11 @@ class HashMap {
     return hashCode;
   }
 
-  // set(key, value) takes two arguments, the first is a key and the second is a value that is assigned to this key.
-  // If a key already exists, then the old value is updated.
+  // set(key) takes a single argument.
+  // If a key already exists, it is not added again.
   // Creates a head node in a new element
   // If there is a hash collision a linked list is created.
-  set(key, value) {
+  set(key) {
     const index = this.hash(key);
     const bucket = this.buckets[index];
 
@@ -35,21 +35,20 @@ class HashMap {
       throw new Error('Trying to access index out of bound');
     }
 
-    // For overwriting a value
+    // If a key already exists, return.
     const node = bucket.find(key);
     if (node) {
-      node.value = value;
+      console.log(`Key: ${key}. This key already exists.`);
+      return;
     } else {
-      // For not overwriting a value - creating a new key value pair
-      bucket.append(key, value);
+      // If a key does not exist. Add it.
+      bucket.append(key);
       this.size++;
     }
 
     if (this.size / this.capacity > this.loadFactor) {
       this.resize();
     }
-
-    // console.log(this.buckets);
     // Grow bucket when need to. When bucket size has reached the load factor.
   }
 
@@ -62,22 +61,19 @@ class HashMap {
 
     // Rehashing: All existing entries are rehashed to map them to new bucket indices based on the new size of the array.
     for (let i = 0; i < this.buckets.length; i++) {
-      // Get the key with the value
+      // Get the key.
       let current = this.buckets[i].head;
       while (current !== null) {
         const newIndex = this.hash(current.key);
-        newBuckets[newIndex].append(current.key, current.value);
+        newBuckets[newIndex].append(current.key);
         current = current.nextNode;
       }
-
-      // Use the hash function to hash it
-      // Use the set function to add it to new array
     }
+    // Reassign Entries: Entries are moved to the new bucket array to reflect the updated hash keys.
     this.buckets = newBuckets;
-    // Reassign Entries: Entries are moved to the new bucket array to reflect the updated hash values.
   }
 
-  // get(key) takes one argument as a key and returns the value that is assigned to this key. If a key is not found, return null.
+  // get(key) takes one argument as a key and returns the key. If a key is not found, return null.
   get(key) {
     const index = this.hash(key);
     const bucket = this.buckets[index];
@@ -89,7 +85,7 @@ class HashMap {
     }
 
     if (node) {
-      return node.value;
+      return node.key;
     }
 
     return null;
@@ -162,33 +158,6 @@ class HashMap {
 
     return allKeys;
   }
-
-  // values() returns an array containing all the values.
-
-  values() {
-    let allValues = [];
-
-    for (let i = 0; i < this.buckets.length; i++) {
-      const bucket = this.buckets[i];
-      const value = bucket.getAllValues();
-      allValues = allValues.concat(value);
-    }
-
-    return allValues;
-  }
-
-  // entries() returns an array that contains each key, value pair. Example: [[firstKey, firstValue], [secondKey, secondValue]]
-  entries() {
-    let allKeyValues = [];
-
-    for (let i = 0; i < this.buckets.length; i++) {
-      const bucket = this.buckets[i];
-      const keyValue = bucket.getAllKeyValues();
-      allKeyValues = allKeyValues.concat(keyValue);
-    }
-
-    return allKeyValues;
-  }
 }
 
-export default HashMap;
+export default HashSet;
